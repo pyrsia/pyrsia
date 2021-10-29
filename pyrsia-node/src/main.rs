2f8c08fc40b6836bf0c5a09cdf25dfdd0f8bcada
Joeri Sykora <joeri@sertik.net>
2021-10-29 15:40:00 +0200

    tcp connection handling with basic threadpool

--- pyrsia-node/src/main.rs
+++ pyrsia-node/src/main.rs
@@ -1,13 +1,35 @@
-use std::io::{self, Read};
+use std::io::prelude::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+mod threading;
+use threading::ThreadPool;
+
+const PORT: &str = "7878";
 
 fn main() {
-    println!("Pyrsia Node is now running!");
-    println!("Press enter to exit...");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
+    println!("Pyrsia Node is now listening on port {}!", PORT);
 
-    let stdin = io::stdin();
-    for _b in stdin.bytes() {
-        break;
+    let threadpool = ThreadPool::new(16).unwrap_or_else(|error| {
+        panic!("Error creating thread pool: {:?}", error.to_string());
+    });
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        threadpool.execute(|| {
+            handle_connection(stream);
+        });
     }
+}
 
-    println!("Pyrsia Node exited.");
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "hi!";
+    println!("Response: {}", response);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
