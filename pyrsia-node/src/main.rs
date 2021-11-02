66342b4843331f863a0d9d1587781a2cbe1137b8
Elliott Frisch <elliottf@jfrog.com>
2021-11-02 08:46:23 -0400

    Added an initial unit test.

--- pyrsia-node/src/main.rs
+++ pyrsia-node/src/main.rs
@@ -122,3 +122,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Ok(())
     })
 }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(1 + 2, 3);
+    }
+}
