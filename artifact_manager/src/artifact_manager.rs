21fef94114692d1389700896706a18e998fea34e
mgrand <mgrand@markgrand.com>
2021-11-06 12:32:47 -0400

    a little bit of code

--- artifact_manager/src/artifact_manager.rs
+++ artifact_manager/src/artifact_manager.rs
@@ -1,3 +1,25 @@
+use std::error::Error;
+use std::fs;
+
+///
+/// # Artifact Manager
+/// Library for managing artifacts. It manages a local collection of artifacts and is responsible
+/// getting artifacts from other nodes when they are not present locally.
+///
+/// Create an ArtifactManager object by passing a path for the local artifact repository to `new`.
+/// ```
+/// new ArtifactManager("/var/lib/pyrsia")
+/// '''
+pub struct ArtifactManager {
+    pub repository_path : string,
+}
+
+impl ArtifactManager {
+    pub fn new(repository_path : string) -> Result<ArtifactManager, &str> {
+        ArtifactManager { repository_path }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
