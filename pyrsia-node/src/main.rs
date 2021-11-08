34de4c960a72fd95e076793a239d798fc4cbaac4
Joeri Sykora <joeri@sertik.net>
2021-11-08 16:56:12 +0100

    #13 start of accepting pushing blobs in chunks

--- pyrsia-node/src/main.rs
+++ pyrsia-node/src/main.rs
@@ -1,17 +1,23 @@
+extern crate bytes;
 extern crate clap;
 extern crate log;
 extern crate pretty_env_logger;
 extern crate tokio;
+extern crate uuid;
 extern crate warp;
 
 use std::convert::Infallible;
 use std::fs;
+use std::io::prelude::*;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
+use bytes::{Buf, Bytes};
 use clap::{App, Arg, ArgMatches};
 use log::{debug, info};
+use uuid::Uuid;
 use warp::Filter;
-use warp::http::HeaderMap;
+use warp::http::{HeaderMap, StatusCode};
+use warp::reject::Reject;
 
 const DEFAULT_PORT: &str = "7878";
 
@@ -75,15 +81,27 @@ async fn main() {
     let v2_blobs = warp::path!("v2" / String / "blobs" / String)
         .and(warp::get().or(warp::head()).unify())
         .and_then(handle_get_blobs);
+    let v2_blobs_create = warp::path!("v2" / String / "blobs" / "uploads")
+        .and(warp::post())
+        .and_then(handle_create_blob);
+    let v2_blobs_patch = warp::path!("v2" / String / "blobs" / "uploads" / String)
+        .and(warp::patch())
+        .and(warp::body::bytes())
+        .and_then(handle_patch_blob);
 
     let routes = warp::any().and(log_headers()).and(
-        v2_base.or(v2_manifests).or(v2_blobs)
+        v2_base.or(v2_manifests).or(v2_blobs).or(v2_blobs_create).or(v2_blobs_patch)
     ).with(warp::log("pyrsia_registry"));
 
     warp::serve(routes)
         .run(address)
         .await;
 }
+
+#[derive(Debug)]
+struct CustomError(String);
+
+impl Reject for CustomError {}
 
 async fn handle_get_manifests(name: String, tag: String) -> Result<impl warp::Reply, warp::Rejection> {
     let colon = tag.find(':');
@@ -109,7 +127,7 @@ async fn handle_get_manifests(name: String, tag: String) -> Result<impl warp::Re
     return Ok(warp::http::response::Builder::new()
         .header("Content-Type", "application/vnd.docker.distribution.manifest.v2+json")
         .header("Content-Length", content.len())
-        .status(200)
+        .status(StatusCode::OK)
         .body(content)
         .unwrap());
 }
@@ -126,9 +144,54 @@ async fn handle_get_blobs(_name: String, hash: String) -> Result<impl warp::Repl
     return Ok(warp::http::response::Builder::new()
         .header("Content-Type", "application/octet-stream")
         .header("Content-Length", content.len())
-        .status(200)
+        .status(StatusCode::OK)
         .body(content)
         .unwrap());
+}
+
+async fn handle_create_blob(name: String) -> Result<impl warp::Reply, warp::Rejection> {
+    let id = Uuid::new_v4();
+
+    if let Err(e) = fs::create_dir_all(format!("/tmp/registry/docker/registry/v2/repositories/{}/_uploads/{}", name, id)) {
+        return Err(warp::reject::custom(CustomError(e.to_string())));
+    }
+
+    Ok(warp::http::response::Builder::new()
+        .header("Location", format!("http://localhost:7878/v2/{}/blobs/uploads/{}", name, id))
+        .status(StatusCode::ACCEPTED)
+        .body("")
+        .unwrap()
+    )
+}
+
+async fn handle_patch_blob(name: String, id: String, mut bytes: Bytes) -> Result<impl warp::Reply, warp::Rejection> {
+    let blob = format!("/tmp/registry/docker/registry/v2/repositories/{}/_uploads/{}/data", name, id);
+    debug!("Patching blob: {}", blob);
+    let file = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(blob);
+    if let Ok(mut f) = file {
+        while bytes.has_remaining() {
+            let bytes_remaining = bytes.remaining();
+            let bytes_to_read = if bytes_remaining <= 4096 { bytes_remaining } else { 4096 };
+            let mut b = vec![0; bytes_to_read];
+            bytes.copy_to_slice(&mut b);
+            if let Err(e) = f.write_all(&b) {
+                return Err(warp::reject::custom(CustomError(e.to_string())));
+            }
+        }
+    } else {
+        let e = file.err().unwrap();
+        return Err(warp::reject::custom(CustomError(e.to_string())));
+    }
+
+    Ok(warp::http::response::Builder::new()
+        .header("Location", format!("http://localhost:7878/v2/{}/blobs/uploads/{}", name, id))
+        .status(StatusCode::ACCEPTED)
+        .body("")
+        .unwrap()
+    )
 }
 
 fn log_headers() -> impl Filter<Extract = (), Error = Infallible> + Copy {
