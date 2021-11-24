d27585ee957876c29f3f0042cc54f19d10a5ee73
mgrand <mgrand@markgrand.com>
2021-11-24 18:29:48 -0500

    added some fn signatures to Signed trait.

--- pyrsia-client-lib/src/lib.rs
+++ pyrsia-client-lib/src/lib.rs
@@ -1,13 +1,3 @@
 /// Library for code that will be use by both Pyrsia nodes and Pyrsia clients.
 
 pub mod signed;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4
-        );
-    }
-}
--- pyrsia-client-lib/src/signed.rs
+++ pyrsia-client-lib/src/signed.rs
@@ -1,23 +1,83 @@
-/// This trait should be implemented by all structs that contain signed data.<br><br>
+extern crate anyhow;
+extern crate ring;
+extern crate serde;
+extern crate serde_json;
+
+use std::option::Option;
+
+use anyhow::Result;
+use ring::signature::RsaKeyPair;
+use serde::{Deserialize, Serialize};
+
+
+/// This trait should be implemented by all structs that contain signed data. Structs that implement
+/// this trait should be annotated with
+/// `#[derive(Serialize, Deserialize)]`
 ///
 /// Pyrsia needs to manage a variety of data related to software artifacts. It will store all of
 /// this data as JSON.  The reason for using JSON is to promote interoperability. If Pyrsia is
 /// successful people will write their own implementations of Pyrsia nodes. For this reason, we
-/// choose standard [JSON](https://www.json.org/json-en.html) .<br>
-///<br>
-/// Pyrsia nodes may store a substantial amount of data. Even if there are gigabytes of data, we
-/// will expect the data to be found and retrieved quickly. For this reason we plan to put JSON
-/// documents in a document store that will index them by values in their fields. In order for a
-/// document store to be able to index JSON documents, they must be in the form of clear text,
-/// rather than being in an encoded or compressed form.<br>
-///<BR>
+/// choose standard [JSON](https://www.json.org/json-en.html) .
+///
 /// All JSON that Pyrsia manages must be signed so that we can attribute it to a source and be
-/// confident that it has not been modified since it was signed. Based on the above rationale, we
-/// have these requirements for how to sign JSON documents:
+/// confident that it has not been modified since it was signed. Because we are using JSON based
+/// signatures, when we deserialize JSON to a struct, to be considered signed, the struct must
+/// contain a reference to the JSON it was deserialized from, so we can still verify the signature.
 ///
-/// * We must be able to sign standard JSON that is represented as a UFT-8 string.
-/// * After we have signed a JSON document, it must still be standard JSON.
-/// * The fields and overall structure of the original JSON document must be unchanged by the fact that it is signed.
+/// If we modify the contents of a signed struct, we should discard the JSON since it no longer
+/// matches the struct. When the struct is in an unsigned state, it should not be allowed to
+/// serialize it.
 ///
+/// Given the above description of the purposes of the `Signed` trait, the descriptions of its
+/// methods should be understood in this context.
 ///
-pub trait Signed {}
+/// It is recommended for consistency that structs that implement this trait have a field declared
+/// like this with a field named `__json` to refer to the struct's json string:
+/// ```
+/// #[derive(Serialize, Deserialize, Debug)]
+/// struct Foo<'a> {
+///   foo: &'a str,
+///   bar: u32,
+///   #[serde(skip)]
+///   __json: Option<&'a str>
+/// }
+/// ```
+pub trait Signed<'a>: Deserialize<'a> + Serialize {
+    /// Return as a string the JSON associated with this struct.
+    fn json(&self) -> Option<String>;
+
+    /// Remove the JSON string from the struct. This should be called by setter methods that modify
+    /// the contents of the struct.
+    fn clear_json(&mut self) ;
+
+    fn from_json_string<T>(json: &str) -> Result<T, anyhow::Error> where T: Signed<'a> {
+        todo!()
+    }
+
+    fn sign(&mut self, algorithm: SignatureAlgorithms, key_pair: &RsaKeyPair) {
+        todo!()
+    }
+
+}
+
+/// An enumeration of the supported signature algorithms
+pub enum SignatureAlgorithms {
+    RsaPkcs1Sha512
+}
+
+#[cfg(test)]
+mod tests {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize)]
+    struct Foo<'a> {
+        #[serde(skip)]
+        __json: Option<&'a str>,
+    }
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
