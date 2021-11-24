bd773d3549aa992fdaf4721a3e5fa7bb30cf9cf5
Elliott Frisch <elliottf@jfrog.com>
2021-11-24 10:36:55 -0500

    Basic working http module implementing get and unit test.

---
+++ pyrsia-node/src/http/mod.rs
@@ -0,0 +1,60 @@
+extern crate reqwest;
+extern crate tokio;
+
+use reqwest::blocking;
+use reqwest::blocking::Response;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufRead;
+
+pub async fn get<W>(mut out: W, url: String) -> Result<u64, std::io::Error>
+where
+    W: Write,
+{
+    let mut resp: Response = blocking::get(url).unwrap();
+    assert!(resp.status().is_success());
+    io::copy(&mut resp, &mut out)
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    fn first_line<R>(mut rdr: R) -> String
+    where
+        R: BufRead,
+    {
+        let mut first_line: String = String::new();
+        rdr.read_line(&mut first_line).expect("Unable to read line");
+        first_line
+    }
+
+    #[test]
+    fn test_get() {
+        use std::fs::File;
+        use std::io::BufReader;
+        let file_name: String = String::from("/tmp/great_expectations.txt");
+
+        let uri: String = "https://www.gutenberg.org/files/1400/1400-0.txt".to_string();
+        let result = get(File::create(file_name.clone()).unwrap(), uri);
+        match futures::executor::block_on(result) {
+            Err(_) => println!("Caught an error"),
+            Ok(_) => println!("Got web page"),
+        }
+
+        let f = File::open(file_name.clone()).unwrap();
+        let first: String = String::from(
+            first_line(BufReader::new(f))
+                .strip_prefix("\u{feff}")
+                .unwrap(),
+        );
+        let right = "The Project Gutenberg eBook of Great Expectations, by Charles Dickens";
+        assert_eq!(first.trim(), right);
+        println!("\u{2705} test passed.");
+        match std::fs::remove_file(file_name.clone()) {
+            Err(e) => println!("Caught error removing temp file {}", e),
+            Ok(_) => println!("Removed temp file"),
+        }
+    }
+}
