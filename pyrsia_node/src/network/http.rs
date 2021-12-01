e5325edf259213f0be2406b804cde95c5eabfe5b
Elliott Frisch <edfrisch@bonobos.com>
2021-12-01 12:17:12 -0500

    Moved http get from utils to network. Cleaned up namespaces a bit. (#61)
    Co-authored-by: Elliott Frisch <elliottf@jfrog.com>

--- pyrsia_node/src/network/http.rs
+++ pyrsia_node/src/network/http.rs
@@ -10,8 +10,7 @@ pub async fn get<W>(mut out: W, url: String) -> anyhow::Result<u64>
 where
     W: Write,
 {
-    use reqwest::blocking::Client;
-    use reqwest::blocking::Response;
+    use reqwest::blocking::{Client, Response};
 
     let client: Client = Client::new();
     let resp: Result<Response, reqwest::Error> = client.get(url.clone()).send();
@@ -25,6 +24,8 @@ where
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
+    use std::fs::File;
+    use std::io::BufReader;
 
     // Reads the first line from a BufRead
     fn first_line<R>(mut rdr: R) -> String
@@ -38,9 +39,6 @@ mod tests {
 
     #[test]
     fn test_get() {
-        use std::fs::File;
-        use std::io::BufReader;
-
         let file_name: String = String::from("/tmp/great_expectations.txt");
         let uri: String = String::from("https://www.gutenberg.org/files/1400/1400-0.txt");
         let result = get(File::create(file_name.clone()).unwrap(), uri);
@@ -49,13 +47,14 @@ mod tests {
             Ok(_) => println!("Got web page"),
         }
 
-        let f = File::open(file_name.clone()).unwrap();
+        let f: File = File::open(file_name.clone()).unwrap();
         let first: String = String::from(
             first_line(BufReader::new(f))
                 .strip_prefix("\u{feff}")
                 .unwrap(),
         );
-        let right = "The Project Gutenberg eBook of Great Expectations, by Charles Dickens";
+        let right: String =
+            String::from("The Project Gutenberg eBook of Great Expectations, by Charles Dickens");
         assert_eq!(first.trim(), right);
         println!("\u{2705} test passed.");
         match std::fs::remove_file(file_name.clone()) {
@@ -66,8 +65,6 @@ mod tests {
 
     #[test]
     fn test_bad_site() {
-        use std::fs::File;
-
         let file_name: String = String::from("/tmp/err.txt");
         let uri: String = String::from("https://nosuchsite.fake/");
         let result = get(File::create(file_name.clone()).unwrap(), uri);
