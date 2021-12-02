e1cee95683cdb84e065b622673101ff35f310bca
Mitali Bisht <mitali.bisht14@gmail.com>
2021-12-02 11:38:01 -0800

    description

--- pyrsia_node/src/node_manager/package.rs
+++ pyrsia_node/src/node_manager/package.rs
@@ -1,7 +1,7 @@
 use super::model::package::{PackageVersion, Package};
 
 //TO BE IMPLEMENETD
-//get_package: provided id and package_version it returns all the metadata for that package version
+//get_package: provided package_name it returns all the metadata for that package version
 pub fn get_package(pkg_name:String) -> Result<Package, anyhow::Error> {
     let pkg = Package{ ..Default::default() };
     Ok(pkg)
