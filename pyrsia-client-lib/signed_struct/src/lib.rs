61f7b879638e5acad0ec99e0273d43338ff3f2aa
mgrand <mgrand@markgrand.com>
2021-12-06 23:37:45 -0500

    some bug fixing

--- pyrsia-client-lib/signed_struct/src/lib.rs
+++ pyrsia-client-lib/signed_struct/src/lib.rs
@@ -14,45 +14,24 @@ use syn::{parse_macro_input, AttributeArgs, DeriveInput, Field, FieldsNamed, Vis
 
 #[proc_macro_attribute]
 pub fn signed_struct(args: TokenStream, input: TokenStream) -> TokenStream {
-    println!("parsing args");
     let _ = parse_macro_input!(args as AttributeArgs);
-    println!("parsing input");
     let mut ast = parse_macro_input!(input as DeriveInput);
-    println!("matching ast");
     match &mut ast.data {
         syn::Data::Struct(ref mut struct_data) => {
-            println!("data matches Struct");
             match &mut struct_data.fields {
                 syn::Fields::Named(fields) => {
-                    println!("Struct contains named fields");
-                    //let json_field_name = unique_json_field_ident(fields);
                     match unique_json_field_ident(fields) {
                         Ok(json_field_name) => {
-                            println!("Field name is {}", json_field_name);
                             let json_field = construct_json_field(&json_field_name);
-                            println!("Constructed field");
                             fields.named.push(json_field);
-                        },
-                        Err(error) => return error.to_compile_error().into()
+                        }
+                        Err(error) => return error.to_compile_error().into(),
                     }
                     println!("generating output");
                     let output = quote! {
                     //#[derive(serde::Serialize, serde::Deserialize)]
+                    #[derive(signed_struct::SignedStructDerive)]
                     #ast
-
-                    // impl<'a> ::pyrsia_client_lib::Signed<'a> for #struct_ident<'a> {
-                    //         pub fn json(&self) -> Option<String> {
-                    //             self.#json_field_name.to_owned()
-                    //         }
-                    //
-                    //         pub fn clear_json(&mut self) {
-                    //             self.#json_field_name = None;
-                    //         }
-                    //
-                    //         fn set_json(&mut self, json: &str) {
-                    //             self.#json_field_name = Option::Some(json.to_string())
-                    //         }
-                    //     };
                     }
                     .into();
                     println!("Output: {}", output);
@@ -69,9 +48,12 @@ pub fn signed_struct(args: TokenStream, input: TokenStream) -> TokenStream {
             }
         }
         _ => {
-            return syn::parse::Error::new(ast.span(), "signed_struct may only be used with structs ")
-                .to_compile_error()
-                .into()
+            return syn::parse::Error::new(
+                ast.span(),
+                "signed_struct may only be used with structs ",
+            )
+            .to_compile_error()
+            .into()
         }
     }
 }
@@ -79,7 +61,6 @@ pub fn signed_struct(args: TokenStream, input: TokenStream) -> TokenStream {
 fn construct_json_field(field_name: &Ident) -> Field {
     let json_fields_named: syn::FieldsNamed = syn::parse2(
         quote!( {
-            #[derivative::Der]
             #field_name : Option<String>
         } )
         .into(),
@@ -93,7 +74,10 @@ fn unique_json_field_ident(fields: &FieldsNamed) -> Result<Ident, syn::parse::Er
     let mut field_names: HashSet<String> = HashSet::new();
     for field in fields.named.iter() {
         if field.vis != Visibility::Inherited {
-            return Err(syn::parse::Error::new(field.span(), "signed_struct requires all fields to be private"));
+            return Err(syn::parse::Error::new(
+                field.span(),
+                "signed_struct requires all fields to be private",
+            ));
         }
         for id in field.ident.iter() {
             field_names.insert(id.to_string());
@@ -104,7 +88,7 @@ fn unique_json_field_ident(fields: &FieldsNamed) -> Result<Ident, syn::parse::Er
         let mut candidate_name = String::from("_json");
         candidate_name.push_str(&counter.to_string());
         if !field_names.contains(candidate_name.as_str()) {
-            return Ok(format_ident!("_json{}", counter.to_string()))
+            return Ok(format_ident!("_json{}", counter.to_string()));
         }
         counter += 1;
     }
@@ -114,9 +98,68 @@ fn unique_json_field_ident(fields: &FieldsNamed) -> Result<Ident, syn::parse::Er
 pub fn signed_struct_derive(input: TokenStream) -> TokenStream {
     println!("parsing input");
     let ast = parse_macro_input!(input as DeriveInput);
-    let struct_ident = &ast.ident;
+    match &ast.data {
+        syn::Data::Struct(ref struct_data) => {
+            println!("data matches Struct");
+            match &struct_data.fields {
+                syn::Fields::Named(fields) => {
+                    println!("Struct contains named fields");
+                    match id_of_last_field(fields) {
+                        Ok(json_field_name) => {
+                            println!("generating output");
+                            let struct_ident = &ast.ident;
+                            let output = quote! {
+                                impl<'a> ::pyrsia_client_lib::Signed<'a> for #struct_ident<'a> {
+                                    fn json(&self) -> Option<String> {
+                                        self.#json_field_name.to_owned()
+                                    }
 
-    quote!( 9 ).into()
+                                    fn clear_json(&mut self) {
+                                        self.#json_field_name = None;
+                                    }
+
+                                    fn set_json(&mut self, json: &str) {
+                                        self.#json_field_name = Option::Some(json.to_string())
+                                    }
+                                }
+
+                            }
+                            .into();
+                            println!("Output: {}", output);
+                            return output;
+                        },
+                        Err(error) => error.to_compile_error().into(),
+                    }
+                }
+                _ => {
+                    return syn::parse::Error::new(
+                        ast.span(),
+                        "signed_struct_derive may only be used with structs having named fields.",
+                    )
+                    .to_compile_error()
+                    .into()
+                }
+            }
+        }
+        _ => {
+            return syn::parse::Error::new(
+                ast.span(),
+                "signed_struct_derive may only be used with structs ",
+            )
+            .to_compile_error()
+            .into()
+        }
+    }
+}
+
+fn id_of_last_field(fields: &FieldsNamed) -> Result<Ident, syn::parse::Error> {
+    match fields.named.last() {
+        Some(field) => Ok(field.ident.clone().unwrap().clone()),
+        None => Err(syn::parse::Error::new(
+            fields.span(),
+            "signed_struct_derive does not work with an empty struct",
+        )),
+    }
 }
 
 #[cfg(test)]
