113516b13527a1b80318a511791924ed1eccf83a
Elliott Frisch <elliottf@jfrog.com>
2021-12-07 09:54:01 -0500

    Remove docker container after testing code coverage. (#76)

--- test_code_coverage.sh
+++ test_code_coverage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t code_coverage:1.0 -f code_coverage_docker .
-docker run --security-opt seccomp=unconfined -it code_coverage:1.0 cargo tarpaulin -v
+docker run --rm --security-opt seccomp=unconfined -it code_coverage:1.0 cargo tarpaulin -v
