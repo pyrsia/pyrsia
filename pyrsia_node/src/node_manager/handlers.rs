a3467b39284d52a55b06ee615537a8a762066d56
Mitali Bisht <mitali.bisht14@gmail.com>
2021-12-10 10:23:51 -0800

    fmt

--- pyrsia_node/src/node_manager/handlers.rs
+++ pyrsia_node/src/node_manager/handlers.rs
@@ -12,8 +12,7 @@ const ART_MGR_DIR: &str = "pyrsia";
 
 lazy_static! {
     static ref ART_MGR: ArtifactManager = {
-        fs::create_dir_all(ART_MGR_DIR)
-            .expect("Error creating dir for artifacts");
+        fs::create_dir_all(ART_MGR_DIR).expect("Error creating dir for artifacts");
         ArtifactManager::new(ART_MGR_DIR).unwrap()
     };
 }
