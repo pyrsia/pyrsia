0ebe6fade49a4967f16d686afed7341af2a22626
Chris Mc <christopherm@jfrog.com>
2021-12-14 19:06:18 -0800

    Add license header to source code (#100)
    * Update rust-lint.yml
    
    * Create license-header-config.json
    
    * Create header-Apache-2.0.txt
    
    * test to see if it passes
    
    * Update and rename .github/license-header-config.json to .github/license-check/config.json
    
    ignore some files
    
    * Update config.json
    
    * update config file path
    
    * add license header
    
    * rustfmt
    
    * whitespace
    
    * fix whitespace for rustfmt

--- pyrsia_node/pyrsia_client_lib/src/lib.rs
+++ pyrsia_node/pyrsia_client_lib/src/lib.rs
@@ -1,3 +1,19 @@
+/*
+   Copyright 2021 JFrog Ltd
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #![allow(mixed_script_confusables)] // This is to allow structs created by a derive macro to have private fields that begin with the grek letter π
 
 /// Library for code that will be use by both Pyrsia nodes and Pyrsia clients.
