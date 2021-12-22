ec95a71a88c56981497f70f52115336545dc96c0
Mitali Bisht <mitalib@jfrog.com>
2021-12-22 11:50:21 -0800

    Update pyrsia_cli/src/commands/node.rs
    Co-authored-by: Chris Mc <prince.chrismc@gmail.com>

--- pyrsia_cli/src/commands/node.rs
+++ pyrsia_cli/src/commands/node.rs
@@ -29,7 +29,7 @@ pub async fn ping() -> Result<String, reqwest::Error> {
             println!("Error: {}", error);
         }
     };
-    //TODO: implement ping api in kademlia
+    //TODO: implement ping api in Node
     let node_url = format!("http://{}/v2", url);
     let response = reqwest::get(node_url).await?.text().await?;
     Ok(response)
