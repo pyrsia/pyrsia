f2c9e3460d82757215c7c6f5eab2454520020fc9
Steve Taylor <steve@deployhub.com>
2022-01-24 16:00:27 -0700

    rename Dockerfiles and use docker actions

--- tests/test_code_coverage.sh
+++ tests/test_code_coverage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --tag codecoverage -f installers/docker/DockerfileCodeCoverageTest .
+docker build --tag codecoverage -f installers/docker/CodeCoverageTest.Dockerfile .
 docker run --rm --security-opt seccomp=unconfined -it codecoverage
