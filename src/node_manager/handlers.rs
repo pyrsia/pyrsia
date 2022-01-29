25978cbb28567502e0026a3d63dd4eddb499319b
Mitali Bisht <mitali.bisht14@gmail.com>
2022-01-29 04:16:27 -0800

    test improvement

--- src/node_manager/handlers.rs
+++ src/node_manager/handlers.rs
@@ -143,10 +143,54 @@ mod tests {
     use std::io::{self, Write};
     use std::fs::File;
 
-    const GOOD_ART_HASH_1: [u8; 32] = [
-        0x12, 0xbf, 0x94, 0xd3, 0xca, 0x1a, 0xe4, 0x9d, 0xe0, 0x8, 0xc6, 0x5b, 0x79, 0xbc, 0x26, 0xf8, 
-        0x47, 0x1d, 0x77, 0x1c, 0x8c, 0x6, 0x47, 0xa3, 0x3b, 0x9d, 0x69, 0x6e, 0xfe, 0x5d, 0x1c, 0x54
-    ];
+    const MANIFEST_V1_JSON: &str = r##"{
+        "name": "hello-world",
+        "tag": "v3.1",
+        "architecture": "amd64",
+        "fsLayers": [
+           {
+              "blobSum": "sha256:5f70bf18a086007016e948b04aed3b82103a36bea41755b6cddfaf10ace3c6ef"
+           },
+           {
+              "blobSum": "sha256:5f70bf18a086007016e948b04aed3b82103a36bea41755b6cddfaf10ace3c6ef"
+           },
+           {
+              "blobSum": "sha256:cc8567d70002e957612902a8e985ea129d831ebe04057d88fb644857caa45d11"
+           },
+           {
+              "blobSum": "sha256:5f70bf18a086007016e948b04aed3b82103a36bea41755b6cddfaf10ace3c6ef"
+           }
+        ],
+        "history": [
+           {
+              "v1Compatibility": "{\"id\":\"e45a5af57b00862e5ef5782a9925979a02ba2b12dff832fd0991335f4a11e5c5\",\"parent\":\"31cbccb51277105ba3ae35ce33c22b69c9e3f1002e76e4c736a2e8ebff9d7b5d\",\"created\":\"2014-12-31T22:57:59.178729048Z\",\"container\":\"27b45f8fb11795b52e9605b686159729b0d9ca92f76d40fb4f05a62e19c46b4f\",\"container_config\":{\"Hostname\":\"8ce6509d66e2\",\"Domainname\":\"\",\"User\":\"\",\"Memory\":0,\"MemorySwap\":0,\"CpuShares\":0,\"Cpuset\":\"\",\"AttachStdin\":false,\"AttachStdout\":false,\"AttachStderr\":false,\"PortSpecs\":null,\"ExposedPorts\":null,\"Tty\":false,\"OpenStdin\":false,\"StdinOnce\":false,\"Env\":[\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"],\"Cmd\":[\"/bin/sh\",\"-c\",\"#(nop) CMD [/hello]\"],\"Image\":\"31cbccb51277105ba3ae35ce33c22b69c9e3f1002e76e4c736a2e8ebff9d7b5d\",\"Volumes\":null,\"WorkingDir\":\"\",\"Entrypoint\":null,\"NetworkDisabled\":false,\"MacAddress\":\"\",\"OnBuild\":[],\"SecurityOpt\":null,\"Labels\":null},\"docker_version\":\"1.4.1\",\"config\":{\"Hostname\":\"8ce6509d66e2\",\"Domainname\":\"\",\"User\":\"\",\"Memory\":0,\"MemorySwap\":0,\"CpuShares\":0,\"Cpuset\":\"\",\"AttachStdin\":false,\"AttachStdout\":false,\"AttachStderr\":false,\"PortSpecs\":null,\"ExposedPorts\":null,\"Tty\":false,\"OpenStdin\":false,\"StdinOnce\":false,\"Env\":[\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"],\"Cmd\":[\"/hello\"],\"Image\":\"31cbccb51277105ba3ae35ce33c22b69c9e3f1002e76e4c736a2e8ebff9d7b5d\",\"Volumes\":null,\"WorkingDir\":\"\",\"Entrypoint\":null,\"NetworkDisabled\":false,\"MacAddress\":\"\",\"OnBuild\":[],\"SecurityOpt\":null,\"Labels\":null},\"architecture\":\"amd64\",\"os\":\"linux\",\"Size\":0}\n"
+           },
+           {
+              "v1Compatibility": "{\"id\":\"e45a5af57b00862e5ef5782a9925979a02ba2b12dff832fd0991335f4a11e5c5\",\"parent\":\"31cbccb51277105ba3ae35ce33c22b69c9e3f1002e76e4c736a2e8ebff9d7b5d\",\"created\":\"2014-12-31T22:57:59.178729048Z\",\"container\":\"27b45f8fb11795b52e9605b686159729b0d9ca92f76d40fb4f05a62e19c46b4f\",\"container_config\":{\"Hostname\":\"8ce6509d66e2\",\"Domainname\":\"\",\"User\":\"\",\"Memory\":0,\"MemorySwap\":0,\"CpuShares\":0,\"Cpuset\":\"\",\"AttachStdin\":false,\"AttachStdout\":false,\"AttachStderr\":false,\"PortSpecs\":null,\"ExposedPorts\":null,\"Tty\":false,\"OpenStdin\":false,\"StdinOnce\":false,\"Env\":[\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"],\"Cmd\":[\"/bin/sh\",\"-c\",\"#(nop) CMD [/hello]\"],\"Image\":\"31cbccb51277105ba3ae35ce33c22b69c9e3f1002e76e4c736a2e8ebff9d7b5d\",\"Volumes\":null,\"WorkingDir\":\"\",\"Entrypoint\":null,\"NetworkDisabled\":false,\"MacAddress\":\"\",\"OnBuild\":[],\"SecurityOpt\":null,\"Labels\":null},\"docker_version\":\"1.4.1\",\"config\":{\"Hostname\":\"8ce6509d66e2\",\"Domainname\":\"\",\"User\":\"\",\"Memory\":0,\"MemorySwap\":0,\"CpuShares\":0,\"Cpuset\":\"\",\"AttachStdin\":false,\"AttachStdout\":false,\"AttachStderr\":false,\"PortSpecs\":null,\"ExposedPorts\":null,\"Tty\":false,\"OpenStdin\":false,\"StdinOnce\":false,\"Env\":[\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"],\"Cmd\":[\"/hello\"],\"Image\":\"31cbccb51277105ba3ae35ce33c22b69c9e3f1002e76e4c736a2e8ebff9d7b5d\",\"Volumes\":null,\"WorkingDir\":\"\",\"Entrypoint\":null,\"NetworkDisabled\":false,\"MacAddress\":\"\",\"OnBuild\":[],\"SecurityOpt\":null,\"Labels\":null},\"architecture\":\"amd64\",\"os\":\"linux\",\"Size\":0}\n"
+           }
+        ],
+        "schemaVersion": 1,
+        "signatures": [
+           {
+              "header": {
+                 "jwk": {
+                    "crv": "P-256",
+                    "kid": "OD6I:6DRK:JXEJ:KBM4:255X:NSAA:MUSF:E4VM:ZI6W:CUN2:L4Z6:LSF4",
+                    "kty": "EC",
+                    "x": "3gAwX48IQ5oaYQAYSxor6rYYc_6yjuLCjtQ9LUakg4A",
+                    "y": "t72ge6kIA1XOjqjVoEOiPPAURltJFBMGDSQvEGVB010"
+                 },
+                 "alg": "ES256"
+              },
+              "signature": "XREm0L8WNn27Ga_iE_vRnTxVMhhYY0Zst_FfkKopg6gWSoTOZTuW4rK0fg_IqnKkEKlbD83tD46LKEGi5aIVFg",
+              "protected": "eyJmb3JtYXRMZW5ndGgiOjY2MjgsImZvcm1hdFRhaWwiOiJDbjAiLCJ0aW1lIjoiMjAxNS0wNC0wOFQxODo1Mjo1OVoifQ"
+           }]}"##;
+
+    const GOOD_ART_HASH: [u8; 32] = [
+    0x9c, 0x9f, 0xe2, 0x3f, 0x63, 0xae, 0x3b, 0x6c, 0x50, 0xe7, 0x85, 0x8, 0xeb, 0x99, 0xd7, 0x9a, 0xae, 
+    0xd3, 0x31, 0x1c, 0xfc, 0x9f, 0x6f, 0xba, 0xce, 0xa4, 0x2d, 0x7b, 0x94, 0x31, 0xa, 0x9f ];
+
+
     #[test]
     fn put_and_get_artifact_test() -> Result<(), anyhow::Error> {
         debug!("put_and_get_artifact_test started !!");
@@ -154,14 +198,14 @@ mod tests {
         let dir = tempdir()?;
         let file_path = dir.path().join("artifact_test.txt");
         let mut file = File::create(file_path.clone())?;
-        writeln!(file, "This is the pyrsia node manager testing data. Pushing and pulling artifacts from node. This is good testing data")?;
+        writeln!(file, "{}",MANIFEST_V1_JSON);
 
         let reader = File::open(file_path.clone()).unwrap();
-        let push_result = put_artifact(&GOOD_ART_HASH_1, Box::new(reader)).context("Error from put_artifact")?;
+        let push_result = put_artifact(&GOOD_ART_HASH, Box::new(reader)).context("Error from put_artifact")?;
         assert_eq!(push_result, true);
 
-        // pull artiafct
-        let file = get_artifact(&GOOD_ART_HASH_1, HashAlgorithm::SHA256)
+        // pull artifact
+        let file = get_artifact(&GOOD_ART_HASH, HashAlgorithm::SHA256)
             .context("Error from get_artifact")?;
 
         //validate pulled artifact with the actual data
@@ -190,21 +234,35 @@ mod tests {
 
     #[test]
     fn test_disk_usage() -> Result<(), anyhow::Error> {
+        fs::remove_dir_all(ART_MGR_DIR).expect(&format!(
+            "unable to remove artifact manager directory {}",
+            ART_MGR_DIR
+        ));
+
+        let usage_pct_before = disk_usage().context("Error from disk_usage")?;
+        assert_eq!("0.0",format!("{:.1}", usage_pct_before));
+
         create_artifact().context("Error creating artifact")?;
 
-        let usage_pct = disk_usage().context("Error from disk_usage")?;
-        assert_eq!("0.000044",format!("{:.6}", usage_pct));
+        let usage_pct_after = disk_usage().context("Error from disk_usage")?;
+        assert_eq!("0.000047",format!("{:.6}", usage_pct_after));
 
         Ok(())
-
     }
 
     #[test]
     fn test_get_space_available() -> Result<(), anyhow::Error> {
+        fs::remove_dir_all(ART_MGR_DIR).expect(&format!(
+            "unable to remove artifact manager directory {}",
+            ART_MGR_DIR
+        ));
+        let space_available_before = get_space_available().context("Error from get_space_available")?;
+        assert_eq!(10840000000, space_available_before);
+
         create_artifact().context("Error creating artifact")?;
         
         let space_available = get_space_available().context("Error from get_space_available")?;
-        assert_eq!(10839995189, space_available);
+        assert_eq!(10839994888, space_available);
 
         Ok(())
 
@@ -213,10 +271,10 @@ mod tests {
         let dir = tempdir()?;
         let file_path = dir.path().join("artifact_test.txt");
         let mut file = File::create(file_path.clone())?;
-        writeln!(file, "This is the pyrsia node manager testing data. Pushing and pulling artifacts from node. This is good testing data")?;
+        writeln!(file, "{}",MANIFEST_V1_JSON);
 
         let reader = File::open(file_path).unwrap();
-        let push_result = put_artifact(&GOOD_ART_HASH_1, Box::new(reader)).context("Error from put_artifact")?;
+        let push_result = put_artifact(&GOOD_ART_HASH, Box::new(reader)).context("Error from put_artifact")?;
 
         assert_eq!(push_result, true);
         Ok(())
