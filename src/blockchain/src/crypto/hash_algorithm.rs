87d28a0008c2c473de965761f6660c2c4f823c5b
Chris McArthur <prince.chrismc@gmail.com>
2022-02-25 17:49:56 -0800

    add tests

--- src/blockchain/src/crypto/hash_algorithm.rs
+++ src/blockchain/src/crypto/hash_algorithm.rs
@@ -25,21 +25,46 @@ pub struct HashDigest {
 }
 
 impl HashDigest {
-    // TODO(prince-chrismc): Define Hash size constant
-
     pub fn new(msg: &[u8]) -> Self {
         Self {
             multihash: Code::Keccak256.digest(msg),
         }
     }
 
-    // TODO(prince-chrismc): Define `to_bytes` for fixed sized array
+    pub fn to_slice(&self) -> [u8; 32] {
+        self.multihash
+            .digest()
+            .try_into()
+            .expect("a valid Keccak256 to be 32 bytes")
+    }
 }
 
 impl aleph_bft::Hasher for HashDigest {
     type Hash = [u8; 32];
 
     fn hash(x: &[u8]) -> Self::Hash {
-        Code::Keccak256.digest(x).to_bytes().try_into().unwrap()
+        HashDigest::new(x).to_slice()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use aleph_bft::Hasher;
+
+    #[test]
+    fn test_hash_digest() {
+        let message = b"hello world";
+        // let message: [u8; 11] = ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'];
+        let expected_digest = [
+            0x47, 0x17, 0x32, 0x85, 0xa8, 0xd7, 0x34, 0x1e, 0x5e, 0x97, 0x2f, 0xc6, 0x77, 0x28,
+            0x63, 0x84, 0xf8, 0x02, 0xf8, 0xef, 0x42, 0xa5, 0xec, 0x5f, 0x03, 0xbb, 0xfa, 0x25,
+            0x4c, 0xb0, 0x1f, 0xad,
+        ];
+
+        let hash = HashDigest::new(message);
+
+        assert_eq!(hash.to_slice(), expected_digest);
+        assert_eq!(HashDigest::hash(message), expected_digest);
     }
 }
