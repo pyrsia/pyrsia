01a60da9882cbb71409acbcf4573822442c22f35
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-03-02 12:59:51 +0530

    Suggestions from Chris
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- pre-commit.bat
+++ pre-commit.bat
@@ -1,6 +1,7 @@
+echo "Welcome to the Pyrsia local tests.\nThis might take sometime, donot interrupt it if the screen is blank."
 cargo install cargo-audit;
 cargo audit;
 cargo clippy;   
 rustup component add rustfmt;
 cargo fmt --check;
-cargo "test";
+cargo test --workspace;
--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,8 @@
 # !/bin/sh
+echo "Welcome to the Pyrsia local tests.\nThis might take sometime, donot interrupt it if the screen is blank."
 cargo install cargo-audit;
 cargo audit;
 cargo clippy;   
 rustup component add rustfmt;
 cargo fmt --check;
-cargo "test";
+cargo "test --workspace";
