e5b5e543eb8404682ace043eb086dab5b0485d21
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-03-04 12:43:13 +0530

    Addition of Cargo clean
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- pre-commit.bat
+++ pre-commit.bat
@@ -1,4 +1,5 @@
 echo "Welcome to the Pyrsia local tests.\nThis might take sometime, donot interrupt it if the screen is blank."
+cargo clean;
 cargo install cargo-audit;
 cargo audit;
 cargo clippy;   
--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,6 @@
 # !/bin/sh
 echo "Welcome to the Pyrsia local tests.\nThis might take sometime, donot interrupt it if the screen is blank."
+cargo clean;
 cargo install cargo-audit;
 cargo audit;
 cargo clippy;   
