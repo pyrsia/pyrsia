09d2b783f32c12cfb1e98d3bf3cea1bcfcecee75
Steve Taylor <steve@deployhub.com>
2022-03-04 12:30:47 -0700

    skip mv of sccache binary

--- .github/workflows/sccache-linux.sh
+++ .github/workflows/sccache-linux.sh
@@ -2,6 +2,5 @@
 
 # Download and install sccache in the Linux specific cargo directories
 mkdir -p /home/runner/.cargo/bin 2>/dev/null
-curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz | tar xzf -
-mv sccache-v0.2.15-x86_64-unknown-linux-musl/sccache /home/runner/.cargo/bin/sccache 
+curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz | tar --strip-components=1 -C /home/runner/.cargo/bin -xzf -
 chmod 755 /home/runner/.cargo/bin/sccache
--- .github/workflows/sccache-macos.sh
+++ .github/workflows/sccache-macos.sh
@@ -2,6 +2,5 @@
 
 # Download and install sccache in the OS/X specific cargo directories
 mkdir -p /Users/runner/.cargo/bin 2>/dev/null
-curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-apple-darwin.tar.gz | tar xzf -
-mv sccache-v0.2.15-x86_64-apple-darwin/sccache /Users/runner/.cargo/bin/sccache 
+curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-apple-darwin.tar.gz | tar --strip-components=1 -C /Users/runner/.cargo/bin -xzf -
 chmod 755 /Users/runner/.cargo/bin/sccache
