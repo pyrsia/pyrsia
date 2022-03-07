4332e852d3f913405449f79e672f82eae269a30b
Sudhindra Rao <sudhindra.r.rao@gmail.com>
2022-03-07 12:14:45 -0800

    Removes expectest - so that we only depend on one testing framework - uses assay to test instead

--- src/docker/v2/storage/blobs.rs
+++ src/docker/v2/storage/blobs.rs
@@ -1,17 +1,17 @@
 /*
-   Copyright 2021 JFrog Ltd
+  Copyright 2021 JFrog Ltd
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 pub use crate::artifacts_repository::hash_util::HashAlgorithm;
@@ -23,95 +23,92 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn append_to_blob(blob: &str, mut bytes: Bytes) -> std::io::Result<(u64, u64)> {
-  debug!("Patching blob: {}", blob);
-  let mut file = fs::OpenOptions::new()
-    .create(true)
-    .append(true)
-    .open(blob)?;
-  let mut total_bytes_read: u64 = 0;
-  let initial_file_length: u64;
-  initial_file_length = file.metadata()?.len();
-  while bytes.has_remaining() {
-    let bytes_remaining = bytes.remaining();
-    let bytes_to_read = if bytes_remaining <= 4096 {
-      bytes_remaining
-    } else {
-      4096
-    };
-    total_bytes_read += bytes_to_read as u64;
-    let mut b = vec![0; bytes_to_read];
-    bytes.copy_to_slice(&mut b);
-    file.write_all(&b)?;
-  }
+    debug!("Patching blob: {}", blob);
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(blob)?;
+    let mut total_bytes_read: u64 = 0;
+    let initial_file_length: u64 = file.metadata()?.len();
+    while bytes.has_remaining() {
+        let bytes_remaining = bytes.remaining();
+        let bytes_to_read = if bytes_remaining <= 4096 {
+            bytes_remaining
+        } else {
+            4096
+        };
+        total_bytes_read += bytes_to_read as u64;
+        let mut b = vec![0; bytes_to_read];
+        bytes.copy_to_slice(&mut b);
+        file.write_all(&b)?;
+    }
 
-  Ok((initial_file_length, total_bytes_read))
+    Ok((initial_file_length, total_bytes_read))
 }
 
 pub fn create_upload_directory(name: &str, id: &str) -> std::io::Result<()> {
-  fs::create_dir_all(format!(
-    "/tmp/registry/docker/registry/v2/repositories/{}/_uploads/{}",
-    name, id
-  ))
+    fs::create_dir_all(format!(
+        "/tmp/registry/docker/registry/v2/repositories/{}/_uploads/{}",
+        name, id
+    ))
 }
 
 pub fn store_blob_in_filesystem(
-  name: &str,
-  id: &str,
-  digest: &str,
-  bytes: Bytes,
+    name: &str,
+    id: &str,
+    digest: &str,
+    bytes: Bytes,
 ) -> Result<bool, Box<dyn std::error::Error>> {
-  let blob_upload_dest_dir = format!(
-    "/tmp/registry/docker/registry/v2/repositories/{}/_uploads/{}",
-    name, id
-  );
-  let mut blob_upload_dest_data = blob_upload_dest_dir.clone();
-  blob_upload_dest_data.push_str("/data");
-  let append = append_to_blob(&blob_upload_dest_data, bytes)?;
+    let blob_upload_dest_dir = format!(
+        "/tmp/registry/docker/registry/v2/repositories/{}/_uploads/{}",
+        name, id
+    );
+    let mut blob_upload_dest_data = blob_upload_dest_dir.clone();
+    blob_upload_dest_data.push_str("/data");
+    let append = append_to_blob(&blob_upload_dest_data, bytes)?;
 
-  // check if there is enough local allocated disk space
-  let available_space = get_space_available(ARTIFACTS_DIR.as_str());
-  if available_space.is_err() {
-    return Err(available_space.err().unwrap().to_string().into());
-  }
-  if append.1 > available_space.unwrap() {
-    return Err("Not enough space left to store artifact".into());
-  }
-  //put blob in artifact manager
-  let reader = File::open(blob_upload_dest_data.as_str()).unwrap();
+    // check if there is enough local allocated disk space
+    let available_space = get_space_available(ARTIFACTS_DIR.as_str());
+    if available_space.is_err() {
+        return Err(available_space.err().unwrap().to_string().into());
+    }
+    if append.1 > available_space.unwrap() {
+        return Err("Not enough space left to store artifact".into());
+    }
+    //put blob in artifact manager
+    let reader = File::open(blob_upload_dest_data.as_str()).unwrap();
 
-  let push_result = put_artifact(
-    hex::decode(&digest.get(7..).unwrap()).unwrap().as_ref(),
-    Box::new(reader),
-    HashAlgorithm::SHA256,
-  )?;
+    let push_result = put_artifact(
+        hex::decode(&digest.get(7..).unwrap()).unwrap().as_ref(),
+        Box::new(reader),
+        HashAlgorithm::SHA256,
+    )?;
 
-  fs::remove_dir_all(&blob_upload_dest_dir)?;
+    fs::remove_dir_all(&blob_upload_dest_dir)?;
 
-  Ok(push_result)
+    Ok(push_result)
 }
 
 #[cfg(test)]
-
 mod tests {
-  use super::*;
-  use bytes::{BufMut, BytesMut};
-  use expectest::expect;
-  use expectest::prelude::*;
+    use super::*;
+    use assay::assay;
+    use bytes::{BufMut, BytesMut};
 
-  #[test]
-  fn append_to_blob_writes_to_filesystem() {
-    let tmp_dir = tempfile::tempdir().unwrap();
-    let path = tmp_dir.path().join("append_to_blob_writes_to_filesystem");
-    let blob_upload_dest_dir = path.to_str().unwrap();
+    #[assay]
+    fn append_to_blob_writes_to_filesystem() {
+        let tmp_dir = tempfile::tempdir().unwrap();
+        let path = tmp_dir.path().join("append_to_blob_writes_to_filesystem");
+        let blob_upload_dest_dir = path.to_str().unwrap();
 
-    let mut buf = BytesMut::with_capacity(20);
-    buf.put(&b"sample_blob"[..]);
-    let bytes = buf.freeze();
-    let length_of_sample_blob: u64 = bytes.len().try_into().unwrap();
+        let mut buf = BytesMut::with_capacity(20);
+        buf.put(&b"sample_blob"[..]);
+        let bytes = buf.freeze();
+        let length_of_sample_blob: u64 = bytes.len().try_into().unwrap();
 
-    let result = append_to_blob(&blob_upload_dest_dir, bytes);
-    let (_, total_bytes_appended) = result.unwrap();
+        let result = append_to_blob(&blob_upload_dest_dir, bytes);
+        let (_, total_bytes_appended) = result.unwrap();
 
-    expect!(total_bytes_appended).to(be_equal_to(length_of_sample_blob));
-  }
+        assert_eq!(total_bytes_appended, length_of_sample_blob);
+    }
 }
