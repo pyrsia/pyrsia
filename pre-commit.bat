46d164733d50383c23571206d8baa2fec1690503
Elliott Frisch <elliottf@jfrog.com>
2022-03-08 08:54:42 -0500

    cargo clippy is working on mac again. keeping batch in sync.

--- pre-commit.bat
+++ pre-commit.bat
@@ -12,6 +12,7 @@ IF %ERRORLEVEL% NEQ 0 (ECHO Could not install cargo-audit.  &Exit /b 1)
 cargo audit
 IF %ERRORLEVEL% NEQ 0 (ECHO Cargo audit failed. &Exit /b 1)
 cargo clippy
+IF %ERRORLEVEL% NEQ 0 (ECHO Cargo clippy failed. &Exit /b 1)
 rustup component add rustfmt
 IF %ERRORLEVEL% NEQ 0 (ECHO Could not install rustfmt. &Exit /b 1)
 cargo fmt --check
--- pre-commit.sh
+++ pre-commit.sh
@@ -8,8 +8,7 @@ fi
 
 cargo install cargo-audit || exit_on_error "Could not install cargo audit."
 cargo audit || exit_on_error "Cargo audit failed."
-# Clippy is failing on macOS right now.
-cargo clippy
+cargo clippy || exit_on_error "Cargo clippy failed."
 rustup component add rustfmt || exit_on_error "Could not install rustfmt."
 cargo fmt --check || exit_on_error "Cargo format failed."
 cargo test --workspace || exit_on_error "Cargo test failed."
