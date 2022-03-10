1496cce2c2c396b70bf22db755439f418bf9b5c2
Joeri Sykora <joeri@sertik.net>
2022-03-10 21:08:21 +0100

    Provide and retrieve artifacts via the kademlia p2p network (#477)
    * lookup blobs via kademlia content provisioning
    * use dial and identify to add new node to dht
    * add local peer_id to status

--- src/docker/v2/routes.rs
+++ src/docker/v2/routes.rs
@@ -19,13 +19,11 @@ use crate::network::p2p;
 
 use super::handlers::blobs::*;
 use super::handlers::manifests::*;
-use libp2p::PeerId;
 use std::collections::HashMap;
 use warp::Filter;
 
 pub fn make_docker_routes(
     p2p_client: p2p::Client,
-    peer_id: Option<PeerId>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let empty_json = "{}";
     let v2_base = warp::path("v2")
@@ -56,7 +54,7 @@ pub fn make_docker_routes(
     let v2_blobs = warp::path!("v2" / "library" / String / "blobs" / String)
         .and(warp::get().or(warp::head()).unify())
         .and(warp::path::end())
-        .and_then(move |name, hash| handle_get_blobs(p2p_client.clone(), peer_id, name, hash));
+        .and_then(move |name, hash| handle_get_blobs(p2p_client.clone(), name, hash));
     let v2_blobs_post = warp::path!("v2" / "library" / String / "blobs" / "uploads")
         .and(warp::post())
         .and_then(handle_post_blob);
