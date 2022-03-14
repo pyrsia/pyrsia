f120d0ffcd3f00bead12866786c17fa77f8f5e69
Steve Taylor <steve@deployhub.com>
2022-03-14 11:28:55 -0600

    add error handling and make RC codes stand out

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -12,17 +12,28 @@
 # Need to save the return codes from the background processes to exit the
 # script with a bad return code if either fails
 
-set -x
+set -e
+
 $( cargo build --workspace --verbose --release; echo $? > /tmp/ws.rc ) &
 $( cargo build --tests --verbose --release 1>/tmp/tests.log 2>&1; echo $? > /tmp/tests.rc ) &
 jobs
 wait
+
+# Display the workspace return code
+echo "### Workspace Build RC=$(cat /tmp/ws.rc)"
 
 # The workspace build is outputing as it runs so we just need to
 # output the test build once they are both done running
 echo cargo build --tests --verbose --release
 cat /tmp/tests.log
 
-# return the max return code between the two processes
+# Display the tests return code
+echo "### Tests Build RC=$(cat /tmp/tests.rc)"
+
+# Return the max return code between the two processes.
+# This is done to tell the GitHub Step to fail (rc != 0). The 
+# value of the rc is not important at this point since each
+# build has already displayed its return code. 
+
 rc=$(cat /tmp/ws.rc /tmp/tests.rc | sort -nr | head -n1)
 exit $rc
--- .github/workflows/sccache-linux.sh
+++ .github/workflows/sccache-linux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
 # Download and install sccache in the Linux specific cargo directories
-mkdir -p /home/runner/.cargo/bin 2>/dev/null
+test -f  /home/runner/.cargo/bin || mkdir -p /home/runner/.cargo/bin 2>/dev/null
 curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz | tar --strip-components=1 -C /home/runner/.cargo/bin -xzf -
 chmod 755 /home/runner/.cargo/bin/sccache
