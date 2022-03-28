687086ef5a057a18c0ea63e37e167d888178097b
Steve Taylor <steve@deployhub.com>
2022-03-28 10:56:29 -0600

    remove test -f sccache install and $PWD from .deb upload

--- .github/workflows/sccache-linux.sh
+++ .github/workflows/sccache-linux.sh
@@ -2,6 +2,6 @@
 
 set -e
 # Download and install sccache in the Linux specific cargo directories
-test -f  /home/runner/.cargo/bin || mkdir -p /home/runner/.cargo/bin 2>/dev/null
+mkdir -p /home/runner/.cargo/bin
 curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz | tar --strip-components=1 -C /home/runner/.cargo/bin -xzf -
 chmod 755 /home/runner/.cargo/bin/sccache
