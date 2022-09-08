84be55a829c4733aaf0d1949a0d334abc611357d
Manas Das <manasd@jfrog.com>
2022-09-08 09:00:50 -0700

    Local docker related changes to add better debug information and more control on entrypoint.

--- tests/test_local_code_coverage.sh
+++ tests/test_local_code_coverage.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-docker compose -f installers/docker/local_code_coverage/docker-compose.yml build
 docker compose -f installers/docker/local_code_coverage/docker-compose.yml up
-docker compose -f installers/docker/local_code_coverage/docker-compose.yml down
+docker compose -f installers/docker/local_code_coverage/docker-compose.yml down --rmi local
