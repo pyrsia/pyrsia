f1ccbda17395624429c4e34628c348aee438aaf8
Manas Das <manasd@jfrog.com>
2022-09-15 14:22:32 -0700

    Pyrsia CLI : "pyrsia config --edit" and "pyrsia config --add" input validation for host, port and disk space

--- pyrsia_cli/src/cli/handlers.rs
+++ pyrsia_cli/src/cli/handlers.rs
@@ -14,29 +14,30 @@
    limitations under the License.
 */
 
+use lazy_static::lazy_static;
 use pyrsia::cli_commands::config;
 use pyrsia::cli_commands::node;
 use pyrsia::node_api::model::cli::{
     RequestDockerBuild, RequestDockerLog, RequestMavenBuild, RequestMavenLog,
 };
+use regex::Regex;
 use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
+use std::net::Ipv4Addr;
 
 const CONF_REMINDER_MESSAGE: &str = "Please make sure the pyrsia CLI config is up to date and matches the node configuration. For more information, run 'pyrsia config --show'";
 
 pub fn config_add() {
-    println!("Enter host: ");
     let mut new_cfg = config::CliConfig {
-        host: io::stdin().lock().lines().next().unwrap().unwrap(),
+        host: read_interactive_input("Enter host: ", &valid_host),
         ..Default::default()
     };
-
-    println!("Enter port: ");
-    new_cfg.port = io::stdin().lock().lines().next().unwrap().unwrap();
-
-    println!("Enter disk space to be allocated to pyrsia(Please enter with units ex: 10 GB): ");
-    new_cfg.disk_allocated = io::stdin().lock().lines().next().unwrap().unwrap();
+    new_cfg.port = read_interactive_input("Enter port: ", &valid_port);
+    new_cfg.disk_allocated = read_interactive_input(
+        "Enter disk space to be allocated to pyrsia(Please enter with units ex: 10 GB): ",
+        &valid_disk_space,
+    );
 
     let result = config::add_config(new_cfg);
     match result {
@@ -161,4 +162,79 @@ pub async fn inspect_maven_transparency_log(gav: &str) {
             println!("Inspect log request failed with error: {:?}", error);
         }
     };
+}
+
+/// Read user input interactively until the validation passed
+fn read_interactive_input<'a>(
+    cli_prompt: &str,
+    validation_func: &'a dyn Fn(&str) -> bool,
+) -> String {
+    loop {
+        println!("{}", cli_prompt);
+        let mut buffer = String::new();
+        match io::stdin().lock().read_line(&mut buffer) {
+            Ok(bytes_read) => {
+                if bytes_read > 0 {
+                    let input = buffer.lines().next().unwrap();
+                    if validation_func(input.clone()) {
+                        break input.to_string();
+                    }
+                }
+            }
+            Err(_) => {}
+        }
+    }
+}
+
+/// Returns true if input is a valid hostname or a valid IPv4 address
+fn valid_host(input: &str) -> bool {
+    /// Returns true if input is a valid hostname as per the definition
+    /// at https://man7.org/linux/man-pages/man7/hostname.7.html, otherwise false
+    fn valid_hostname(input: &str) -> bool {
+        if input.len() == 0 || input.len() > 253 {
+            return false;
+        }
+        lazy_static! {
+            static ref HOSTNAME_REGEX: Regex = Regex::new(r"^(([a-zA-Z0-9]{1,63}|[a-zA-Z0-9][a-zA-Z0-9\-]{0,62})\.)*([a-zA-Z0-9]{1,63}|[a-zA-Z0-9][a-zA-Z0-9\-]{0,62})$").unwrap();
+        }
+        HOSTNAME_REGEX.is_match(input)
+    }
+
+    /// Returns true if input is a valid IPv4 address, otherwise false
+    fn valid_ipv4_address(input: &str) -> bool {
+        match input.parse::<Ipv4Addr>() {
+            Ok(_) => true,
+            Err(_) => false,
+        }
+    }
+
+    valid_ipv4_address(input) || valid_hostname(input)
+}
+
+fn valid_port(input: &str) -> bool {
+    match input.parse::<u16>() {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+fn valid_disk_space(input: &str) -> bool {
+    const DISK_SPACE_NUM_MIN: u16 = 0;
+    const DISK_SPACE_NUM_MAX: u16 = 4096;
+    lazy_static! {
+        static ref DISK_SPACE_RE: Regex = Regex::new(r"^([0-9]{1,4})\s+(GB)$").unwrap();
+    }
+    // let disk_space_re: Regex = Regex::new(r"^([0-9]{1,4})(\s*)(GB)$").unwrap();
+    if DISK_SPACE_RE.is_match(input) {
+        let captured_groups = DISK_SPACE_RE.captures(input).unwrap();
+        let disk_space_num = captured_groups
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse::<u16>()
+            .unwrap();
+        DISK_SPACE_NUM_MIN < disk_space_num && disk_space_num <= DISK_SPACE_NUM_MAX
+    } else {
+        false
+    }
 }
