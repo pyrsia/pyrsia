06cd6928b9533da0b3e936933a9a5a55931fb48a
Steve Taylor <steve@deployhub.com>
2022-09-16 09:18:45 -0600

    Fix logic for first boot node and ca-certificates error in docker build

--- installers/docker/node-entrypoint.sh
+++ installers/docker/node-entrypoint.sh
@@ -12,12 +12,15 @@ if [ -f /var/run/secrets/kubernetes.io/serviceaccount/namespace ]; then
     export PYRSIA_EXTERNAL_IP=$(curl -s --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/namespaces/${NAMESPACE}/services/${PODNAME} | jq -r ".status.loadBalancer.ingress[0].ip")
     echo "PYRSIA_EXTERNAL_IP=$PYRSIA_EXTERNAL_IP"
 
-    # detemine if I am node-0 if so be the primary boot node
-    if [ "$(hostname | rev | cut -c -2 | rev)" = "-0" ]; then
+    # detemine if I am pyrsia-node-0.pyrsia.link (first boot node ever) if so be the primary boot node
+    # need to use wget since nslookup, dig and ping are not installed
+    if wget --timeout=2 -v pyrsia-node-0.pyrsia.link 2>&1 | grep Connecting | grep -q "${PYRSIA_EXTERNAL_IP}" ; then  
         /usr/bin/pyrsia_node $* --listen-only true  
     else
-        /usr/bin/pyrsia_node $* 
+        # I am not pyrsia-node-0.pyrsia.link so use boot.pyrsia.link for boot address and connect to it
+        /usr/bin/pyrsia_node $*
     fi
 else
-    /usr/bin/pyrsia_node $* 
+    # not running under k8s so just startup in default mode
+    /usr/bin/pyrsia_node $*
 fi
