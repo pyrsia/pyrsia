451e159c8d5c0fe58672f81fa10c165722dd4b1c
José Pereda <jose.pereda@gluonhq.com>
2022-10-04 17:03:00 +0200

    1011 Create Windows installer (#1117)
    * 1011 windowsmsi (#1)
    
    Create Windows installer
    
    * Fix format
    
    * fix failing test
    
    * add more debug info
    
    * Provide more detailed readme
    
    * Add installer without service

---
+++ .github/workflows/sccache-windows.bat
@@ -0,0 +1,1 @@
+curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.3.0/sccache-v0.3.0-x86_64-pc-windows-msvc.tar.gz | tar --strip-components=1 -C C:\Users\runneradmin\.cargo\bin -xzf -
