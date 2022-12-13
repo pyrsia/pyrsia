fd69ba3265a52fa9b02d326a16e59f740f21961c
Elliott Frisch <elliottf@jfrog.com>
2022-12-13 15:33:54 -0500

    Tuning the parameters for gossipsub.

--- src/network/event_loop.rs
+++ src/network/event_loop.rs
@@ -784,6 +784,8 @@ mod tests {
             .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
             .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
             .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+            .mesh_n_low(1)
+            .support_floodsub()
             .build()
             .expect("Valid config");
         let mut gossip_sub = Gossipsub::new(
